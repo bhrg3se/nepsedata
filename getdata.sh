c067f8eb2c7565d1f8e32d2238ac4916ff70a9c6
bhrg3se <bhargab@seknox.com>
2021-01-18 21:22:07 +0545

    initial commit

---
+++ getdata.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+DATE=$(date +%m-%d-%Y)
+echo $DATE
+
+trade_qty=$(curl https://newweb.nepalstock.com.np/api/nots/top-ten/trade-qty)
+supplydemand=$(curl https://newweb.nepalstock.com.np/api/nots/nepse-data/supplydemand)
+market_summary=$(curl https://newweb.nepalstock.com.np/api/nots/market-summary)
+turnover=$(curl https://newweb.nepalstock.com.np/api/nots/top-ten/turnover)
+top_gainer=$(curl https://newweb.nepalstock.com.np/api/nots/top-ten/top-gainer)
+nepse_index=$(curl https://newweb.nepalstock.com.np/api/nots/nepse-index)
+floorsheet=$(curl "https://newweb.nepalstock.com.np/api/nots/nepse-data/floorsheet?&sort=contractId,desc")
+trading_average=$(curl https://newweb.nepalstock.com.np/api/nots/nepse-data/trading-average?nDays=120)
+
+echo "$trade_qty"
+echo "{\"trade_qty\":$trade_qty,\"supplydemand\":$supplydemand,\"market_summary\":$market_summary,\"turnover\":$turnover,\"top_gainer\":$top_gainer,\"nepse_index\":$nepse_index,\"floorsheet\":$floorsheet,\"trading_average\":$trading_average}" > data/$DATE.json
